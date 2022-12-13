28eee01c4e09a842462f518e075bbc0be15155d7
Jinny You <baram991103@gmail.com>
2022-12-14 02:24:26 +0900

    Add json DTO for `TextModel`

--- lib/src/model/text_model.dart
+++ lib/src/model/text_model.dart
@@ -17,4 +17,42 @@ class TextModel {
       required this.textAlign,
       required this.scale,
       required this.left});
+
+  TextModel.fromJson(Map<String, dynamic> data)
+    : name = data['text'] ?? '',
+      textStyle = TextStyle(
+        fontFamily: data['textStyle']!['font'],
+        fontStyle: FontStyle.values[data['textStyle']!['fontStyle'] ?? 0],
+        fontWeight: FontWeight.values[data['textStyle']!['fontWeight'] ?? 0],
+        fontSize: data['textStyle']!['fontSize'],
+        height: data['textStyle']!['height'],
+        letterSpacing: data['textStyle']!['letterSpacing'],
+        color: data['textStyle']!['color'] != null ? Color(data['textStyle']!['color']) : null,
+        backgroundColor: data['textStyle']!['backgroundColor'] != null ? Color(data['textStyle']!['backgroundColor']) : null,
+      ),
+      top = data['top'] ?? 0,
+      left = data['left'] ?? 0,
+      scale = data['scale'] ?? 1,
+      isSelected = false,
+      textAlign = TextAlign.values[data['textAlign'] ?? 0];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': name,
+      'top': top,
+      'left': left,
+      'scale': scale,
+      'textStyle': {
+        'font': textStyle.fontFamily,
+        'fontStyle': textStyle.fontStyle?.index ?? 0,
+        'fontWeight': textStyle.fontWeight?.index ?? 0,
+        'fontSize': textStyle.fontSize,
+        'height': textStyle.height,
+        'letterSpacing': textStyle.letterSpacing,
+        'color': textStyle.color?.value,
+        'backgroundColor': textStyle.backgroundColor?.value,
+      },
+      'textAlign': textAlign.index
+    };
+  }
 }
