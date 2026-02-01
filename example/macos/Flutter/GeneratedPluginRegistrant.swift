3df017fb3b81ff83a06d115a2776af4a8f11e182
Jinny You <baram991103@gmail.com>
2026-02-02 04:02:11 +0900

    test

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import dotlottie_flutter
 import path_provider_foundation
 import sqflite_darwin
 import sticker_editor_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DotLottieFlutterPlugin.register(with: registry.registrar(forPlugin: "DotLottieFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   StickereditorPlugin.register(with: registry.registrar(forPlugin: "StickereditorPlugin"))
